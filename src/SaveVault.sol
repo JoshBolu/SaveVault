7a8ee92038c32ba037ec925433542844e5772ab9
dev0xjosh <bsuyiajayi@gmail.com>
2025-10-24 23:52:57 +0100

    second commit

---
+++ src/SaveVault.sol
@@ -0,0 +1,32 @@
+// Layout of Contract:
+// license
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+// SPDX-License-Identifier: MIT SEE LICENSE IN LICENSE
+pragma solidity ;
+
+contract  SaveVault {
+
+    function createVault() external{
+        
+    }
+}
